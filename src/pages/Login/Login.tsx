c6e6583858a028282826487df307b45c3e6cebd9
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-11-08 13:43:00 -0300

    feat: Adiciona página implementando navegação condicional de acordo com a seleção do perfil

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,33 @@
 import InputBar from '../../components/InputBar/InputBar';
 import Logo2 from '../../assets/logo/logo-re-colhe2.png';
+import voltar from '../../assets/icons/voltar.png';
 import NavButton from '../../components/NavButton/NavButton';
 import './Login.css';
+import { useLocation } from 'react-router-dom';
 
 const Login = () => {
+  // Captura o estado enviado pelo Link
+  const location = useLocation();
+  const state = location.state as { userType?: 'morador' | 'empresa' };
+  const userType = state?.userType;
+
   return (
     <>
+      <div>
+        <NavButton to="/selecionar-perfil"><img src={voltar} alt="Volvar para seleção de tipo de perfil" /></NavButton>
+      </div>
       <img src={Logo2} alt="Logo Re.Colhe, é o símbolo de reciclágem mas com estilo da natureza, formados por folhas de planta" />
-      <h2>Bem vindos ao Re.Colhe!</h2>
+
+      {/* Saudação dinâmica */}
+      {userType ? (
+        <h2>Olá {userType}. Bem vindo ao Re.Colhe!</h2>
+      ) : (
+        <h2>Bem vindos ao Re.Colhe!</h2>
+      )}
+
       <h1>Login</h1>
       <h3>Entre com as suas credenciais</h3>
+
       <section>
         <label htmlFor="">E-mail
           <InputBar placeholder="Digite seu e-mail" className="login-input" disabled={true} />
